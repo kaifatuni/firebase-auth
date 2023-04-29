58117a59ba1ec9bdc796a4e204600f43adcd4b32
Mohammad Kaif <kaif.201mn033@nitk.edu.in>
2023-04-29 08:56:31 +0000

    firebase basic

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { AuthProvider } from '@/hooks/useAuth'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+  )
 }
+
+
+export default App
